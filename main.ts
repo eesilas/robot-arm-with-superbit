430173dafcf97ea0ea614b3f819729562692e833
Learn STEM by Doing <32452061+eesilas@users.noreply.github.com>
2024-11-14 17:28:55 +0800

    Add huskylens

--- main.ts
+++ main.ts
@@ -1,14 +1,33 @@
+input.onButtonPressed(Button.A, function () {
+    aa = 1
+})
+input.onButtonPressed(Button.AB, function () {
+    if (huskylens.isAppear_s(HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+        if (huskylens.isAppear(1, HUSKYLENSResultType_t.HUSKYLENSResultBlock)) {
+            x = huskylens.readeBox(1, Content1.xCenter)
+            y = huskylens.readeBox(1, Content1.yCenter)
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    aa = 0
+})
+let y = 0
+let x = 0
+let aa = 0
 basic.showIcon(IconNames.Rollerskate)
 huskylens.initI2c()
 huskylens.initMode(protocolAlgorithm.ALGORITHM_OBJECT_TRACKING)
 radio.setGroup(137)
 basic.forever(function () {
-    for (let index = 0; index <= 120; index++) {
-        SuperBit.Servo(SuperBit.enServo.S1, index)
-        basic.pause(200)
-    }
-    for (let index = 0; index <= 120; index++) {
-        SuperBit.Servo(SuperBit.enServo.S2, index)
-        basic.pause(200)
+    if (aa == 1) {
+        for (let index = 0; index <= 120; index++) {
+            SuperBit.Servo(SuperBit.enServo.S1, index)
+            basic.pause(200)
+        }
+        for (let index = 0; index <= 120; index++) {
+            SuperBit.Servo(SuperBit.enServo.S2, index)
+            basic.pause(200)
+        }
     }
 })
